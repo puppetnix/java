148595b29dc6bb85fc55bd849afab86b88f6c972
andreiskill <andrey.riabin@gmail.com>
2015-07-02 19:47:51 +0300

    Update install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,8 @@
 class java::install  inherits java::params {
 
   if $::osfamily == Debian {
-    # set licese for Oracle Java
-    exec {'set-licence-seen':
+    # set license for Oracle Java
+    exec {'set-license-seen':
       command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections',
       onlyif  => '/usr/bin/debconf-get-selections | /bin/grep "accepted-oracle-license-v1-1"|/bin/grep "true"|/usr/bin/wc -l|/bin/grep 0',
     }
